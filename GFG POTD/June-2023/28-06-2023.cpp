e4796d11fad8117ce4dac3c33dc3d744d0924937
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2023-06-28 10:15:12 +0530

    GFG

---
+++ GFG POTD/June-2023/28-06-2023.cpp
@@ -0,0 +1,11 @@
+class Solution{
+  public:
+    int maxDepth(Node *root) {
+         if (root == nullptr) {
+              return 0;
+          }
+        int left_depth = maxDepth(root->left);
+        int right_depth = maxDepth(root->right);
+        return max(left_depth, right_depth) + 1;
+    }
+};
